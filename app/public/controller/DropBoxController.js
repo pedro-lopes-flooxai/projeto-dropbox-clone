43da505972ae7273ae59086472c99b239c565454
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-03-28 13:14:08 -0300

    feat:  delete storage firebase

--- app/public/controller/DropBoxController.js
+++ app/public/controller/DropBoxController.js
@@ -27,23 +27,120 @@ class DropBoxController {
     return this.listFilesEl.querySelectorAll(".selected");
   }
 
+  removeFolderTask(ref, name){
+
+    return new Promise((resolve, reject)=>{
+
+      let.folderRef = this.getFirebaseRef(ref + '/' + name);
+
+      folderRef.on('value', snapshot =>{
+
+        folderRef.off('value');
+
+        snapshot.forEach(item => {
+
+         let data = item.val();
+         data.key = item.key;
+
+         if (data.type === 'folder') {
+
+          this.removeFolderTask(ref + '/' + name, data.name).then(()=>{
+
+            resolver({
+
+              fields:{
+                key: data.key
+              }
+
+            });
+
+          }).catch(err =>{
+            reject(err);
+          });
+
+         } else if(data.type) {
+
+          this.removeFile(ref + '/' + name, data.name).then(()=>{
+
+            resolver({
+
+              fields:{
+                key: data.key
+              }
+
+            });
+
+          }).catch(err =>{
+            reject(err);
+          });
+
+         }
+          
+        });
+
+        folderRef.remove();
+        
+
+        
+
+      });
+
+    });
+
+  }
+
   removeTask() {
     let promises = [];
 
     this.getSelection().forEach((li) => {
+
       let file = JSON.parse(li.dataset.file);
       let key = li.dataset.key;
 
-      let formData = new FormData()
 
-      formData.append('path', file.path);
-      formData.append('key', key);
+      promises.push(new Promise((resolve, reject)=>{
 
-      promises.push(this.ajax('/file', 'DELETE', formData));
+        if (file.type === 'folder') {
+
+            this.removeFolderTask(this.currentFolder.join('/'), file.name).then(()=>{
+
+              resolve({
+                fields:{
+                     key
+                }
+              });
+      
+            });
+  
+
+        } else if (file.type){
+
+          this.removeFile(this.currentFolder.join('/'), file.name).then(()=>{
+
+            resolve({
+              fields:{
+                   key
+              }
+            });
+    
+          });
+
+        }
+
+
+      }));
 
     });
     
     return Promise.all(promises);
+  }
+
+  removeFile(ref, name){
+
+    let fileRef = firebase.storage().ref(ref).child(name);
+
+   return fileRef.delete();
+
   }
 
   initEvents() {
